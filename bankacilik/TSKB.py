34ded6b4a93fac312c9937621a3eed4ea6005e62
elifkendaz <elifkendaz@yahoo.com.tr>
2020-11-10 15:40:21 +0300

    HATALARIMI DÜZELTTİM

--- bankacilik/TSKB.py
+++ bankacilik/TSKB.py
@@ -18,7 +18,7 @@ style.use('ggplot')
 x = datetime.datetime.now()
 print(x)
 
-df_reversed= pd.read_csv('DataSets\BANKACILIK\TSKB.csv')
+df_reversed= pd.read_csv('BANKACILIK1\TSKB.csv')
 df =df_reversed[::-1]
 print(df.isnull().sum().sum())
 
